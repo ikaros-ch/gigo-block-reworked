eaf069721c0986bd75f6cd2a177240799edf86cc
ikaros-ch <154591960+ikaros-ch@users.noreply.github.com>
2023-12-22 13:44:11 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,11 +11,11 @@ namespace sensors {
         	//% block="H"
        		H
     	}
-	//% blockId=DDMmotor block="Motor %motor|direction of Mcontrol(0~1) %McontrolValue|MSpeed pin %MSpeedPin|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
-    //% McontrolValue.min=0 McontrolValue.max=1 
+	//% blockId=DDMmotor block="Motor %motor|direction (0~1) %McontrolValue|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
+    	//% McontrolValue.min=0 McontrolValue.max=1 
 	//% MSpeedValue.min=0 MSpeedValue.max=255
-    //% motor.fieldEditor="gridpicker" Mmotor.fieldOptions.columns=4
-    //% motor.fieldOptions.tooltips="false" motor.fieldOptions.width="300"
+    	//% motor.fieldEditor="gridpicker" Mmotor.fieldOptions.columns=4
+    	//% motor.fieldOptions.tooltips="false" motor.fieldOptions.width="300"
 	
 
 
