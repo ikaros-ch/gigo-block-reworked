1f42c5d88f9af71efeb289c43ec511a87d72afd6
ikaros-ch <154591960+ikaros-ch@users.noreply.github.com>
2023-12-22 12:24:22 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=0 color=#bf9c32 icon="\uf2db" block="GIGO DDM motor"
+//% weight=0 color=#bf9c32 icon="gigo-engine.png" block="GIGO DDM motor"
 namespace sensors {
 	//% blockId=DDMmotor block="Mcontrol pin %McontrolPin|direction of Mcontrol(0~1) %McontrolValue|MSpeed pin %MSpeedPin|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
     //% McontrolValue.min=0 McontrolValue.max=1 
