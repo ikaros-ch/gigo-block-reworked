089966c6cc943377b4aadfed8ba19408534faa72
ikaros-ch <154591960+ikaros-ch@users.noreply.github.com>
2023-12-27 11:50:56 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,11 +11,10 @@ namespace sensors {
         	//% block="H"
        		H
     	}
-	//% blockId=DDMmotor block="Motor %motor|direction (0~1) %McontrolValue|speed of MSpeed(0~255) %MSpeedValue" blockExternalInputs=false
+	//% blockId=DDMmotor block="Motor %motor|direction (0~1) %McontrolValue|speed of MSpeed(0~100) %MSpeedValue" blockExternalInputs=false
     	//% McontrolValue.min=0 McontrolValue.max=1 
-	//% MSpeedValue.min=0 MSpeedValue.max=255
-    	//% motor.fieldEditor="gridpicker" Mmotor.fieldOptions.columns=1
-    	//% motor.fieldOptions.tooltips="false" motor.fieldOptions.width="300"
+	//% MSpeedValue.min=0 MSpeedValue.max=100
+
 	
 
 
@@ -26,19 +25,19 @@ namespace sensors {
 	        switch (motor) {
             case MotorList.E:
                 pins.analogWritePin(AnalogPin.P15, pins.map(McontrolValue, 0, 1, 0, 1023));
-        	pins.analogWritePin(AnalogPin.P16, pins.map(MSpeedValue, 0, 255, 0, 1023));
+        	pins.analogWritePin(AnalogPin.P16, pins.map(MSpeedValue, 0, 100, 0, 1023));
 		break;
             case MotorList.F:
               	pins.analogWritePin(AnalogPin.P13, pins.map(McontrolValue, 0, 1, 0, 1023));
-        	pins.analogWritePin(AnalogPin.P14, pins.map(MSpeedValue, 0, 255, 0, 1023));
+        	pins.analogWritePin(AnalogPin.P14, pins.map(MSpeedValue, 0, 100, 0, 1023));
 		break;
             case MotorList.G:
                 pins.analogWritePin(AnalogPin.P12, pins.map(McontrolValue, 0, 1, 0, 1023));
-        	pins.analogWritePin(AnalogPin.P2, pins.map(MSpeedValue, 0, 255, 0, 1023));
+        	pins.analogWritePin(AnalogPin.P2, pins.map(MSpeedValue, 0, 100, 0, 1023));
 		break;
             case MotorList.H:
                 pins.analogWritePin(AnalogPin.P8, pins.map(McontrolValue, 0, 1, 0, 1023));
-        	pins.analogWritePin(AnalogPin.P1, pins.map(MSpeedValue, 0, 255, 0, 1023));
+        	pins.analogWritePin(AnalogPin.P1, pins.map(MSpeedValue, 0, 100, 0, 1023));
 		break;
                 }    
 	    
