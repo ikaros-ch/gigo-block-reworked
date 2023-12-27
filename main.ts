87e1e5af17530a9e41720366789b210a0ebcaec7
ikaros-ch <154591960+ikaros-ch@users.noreply.github.com>
2023-12-27 13:09:36 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,61 +2,52 @@
 namespace sensors {
 
 	export enum MotorList {
-        	//% block="E"
-        	E,
-        	//% block="F"
-        	F,
-        	//% block="G"
-        	G,
-        	//% block="H"
-       		H
-    	}
-	//% blockId=setmotorspeed block=" Set Motor %motor Speed %speed" blockExternalInputs=false
-   
+		//% block="E"
+		E,
+		//% block="F"
+		F,
+		//% block="G"
+		G,
+		//% block="H"
+		H
+	}
+
+	//% blockId=setmotorspeed block="Set Motor %motor Speed %speed" blockExternalInputs=false
 	//% speed.min=-100 speed.max=100
-
-	
-
-
-
-    
-    export function DDMmotor(motor: MotorList,speed: number): void {
-
-	        switch (motor) {
-            case MotorList.E:
-		if (speed >= 0){
-			pins.digitalWritePin(DigitalPin.P15, 0)
-		else if (speed < 0){
-			pins.digitalWritePin(DigitalPin.P15, 1)
+	export function DDMmotor(motor: MotorList, speed: number): void {
+		switch (motor) {
+			case MotorList.E:
+				if (speed >= 0) {
+					pins.digitalWritePin(DigitalPin.P15, 0);
+				} else if (speed < 0) {
+					pins.digitalWritePin(DigitalPin.P15, 1);
+				}
+				pins.analogWritePin(AnalogPin.P16, pins.map(Math.abs(speed), 0, 100, 0, 1023));
+				break;
+			case MotorList.F:
+				if (speed >= 0) {
+					pins.digitalWritePin(DigitalPin.P13, 0);
+				} else if (speed < 0) {
+					pins.digitalWritePin(DigitalPin.P13, 1);
+				}
+				pins.analogWritePin(AnalogPin.P14, pins.map(Math.abs(speed), 0, 100, 0, 1023));
+				break;
+			case MotorList.G:
+				if (speed >= 0) {
+					pins.digitalWritePin(DigitalPin.P12, 0);
+				} else if (speed < 0) {
+					pins.digitalWritePin(DigitalPin.P12, 1);
+				}
+				pins.analogWritePin(AnalogPin.P2, pins.map(Math.abs(speed), 0, 100, 0, 1023));
+				break;
+			case MotorList.H:
+				if (speed >= 0) {
+					pins.digitalWritePin(DigitalPin.P8, 0);
+				} else if (speed < 0) {
+					pins.digitalWritePin(DigitalPin.P8, 1);
+				}
+				pins.analogWritePin(AnalogPin.P1, pins.map(Math.abs(speed), 0, 100, 0, 1023));
+				break;
 		}
-		pins.analogWritePin(AnalogPin.P16, pins.map(Math.abs(speed), 0, 100, 0, 1023));
-		break;
-            case MotorList.F:
-              	if (speed >= 0){
-			pins.digitalWritePin(DigitalPin.P13, 0)
-		else if (speed < 0){
-			pins.digitalWritePin(DigitalPin.P13, 1)
-		}
-        	pins.analogWritePin(AnalogPin.P14, pins.map(Math.abs(speed), 0, 100, 0, 1023));
-		break;
-            case MotorList.G:
-              	if (speed >= 0){
-			pins.digitalWritePin(DigitalPin.P12, 0)
-		else if (speed < 0){
-			pins.digitalWritePin(DigitalPin.P12, 1)
-		}
-        	pins.analogWritePin(AnalogPin.P2, pins.map(Math.abs(speed), 0, 100, 0, 1023));
-		break;
-            case MotorList.H:
-              	if (speed >= 0){
-			pins.digitalWritePin(DigitalPin.P8, 0)
-		else if (speed < 0){
-			pins.digitalWritePin(DigitalPin.P8, 1)
-		}
-        	pins.analogWritePin(AnalogPin.P1, pins.map(Math.abs(speed), 0, 100, 0, 1023));
-		break;
-                }    
-	    
-   
-    }
+	}
 }
